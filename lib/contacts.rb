c6ddc011ed8d77d0ae7c65ba04cd076ff9c9f5dd
PARDEEP KAUR <pkaur84@aol.com>
2020-05-13 05:38:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,10 +15,21 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
+  contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
-        data.delete_if { |ice_cream| ice_cream == "strawberry"}
-      end
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+     end 
     end
+  end
+end
+
+remove_strawberry(contacts)
+
+puts contacts ["Jon Snow"] [:email]
+puts "//////"
+puts contacts ["Jon Snow"].keys
+puts contacts ["Jon Snow"].values
+
+
   
